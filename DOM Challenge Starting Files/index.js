a04131347704f3b593674f3ec789faa0e57a31b8
manish aditya <m.aditya@myyogateacher.com>
2025-01-15 21:50:39 +0530

    drum project

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+// document.querySelector("button").classList.toggle("invisible");
+
+
+
+var paragraph = document.getElementById("myParagraph");
+paragraph.addEventListener("mouseover",function (){
+paragraph.style.color= "red";
+})
---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,75 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// Detecting button clicks
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+// Detecting key presses
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        default:
+            console.log("Unassigned key: " + key);
+    }
+}
+
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    // Check if activeButton exists before adding/removing class
+    if (activeButton) {
+        activeButton.classList.add("pressed");
+
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+
+}
